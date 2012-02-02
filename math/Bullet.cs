2628fb293dbb111a38c2647ac342a9d45228f2a7
alex zhang <alexzhangh@gmail.com>
2012-02-01 20:11:39 -0800

    initial commit

---
+++ math/Bullet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace math
+{
+    class Bullet
+    {
+        public static List<Bullet> toremove = new List<Bullet>();
+        public static List<Bullet> bullets = new List<Bullet>();
+            public double angle;
+            public Color col;
+            public Vector2 pos;
+            public double speed = 8;
+        
+    }
+}

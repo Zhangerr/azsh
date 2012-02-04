1b229e0e5dbf8bec78aed45e194ecb7e2de1d93e
alex zhang <alexzhangh@gmail.com>
2012-02-03 22:54:36 -0800

    added hp and inertia to homing

--- math/Bullet.cs
+++ math/Bullet.cs
@@ -17,6 +17,9 @@ namespace math
         Enemy lockOn;
         bool found = false;
         public bool tracking;
+        Vector2 inertia = new Vector2(float.NaN, float.NaN);
+        bool initi = true;
+        int inertiafact = 1;
         public void update()
         {
             if (!Program.theGame.Window.ClientBounds.Contains((int)pos.X + Program.theGame.Window.ClientBounds.X, (int)pos.Y + Program.theGame.Window.ClientBounds.Y))
@@ -26,7 +29,7 @@ namespace math
             Vector2 vec = new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle));
             if (lockOn == null && !found && tracking)
             {
-                float smallest = 999;
+                float smallest = float.MaxValue;
                 Enemy temp = null;
                 foreach (Enemy e in Enemy.enemies)
                 {
@@ -47,10 +50,43 @@ namespace math
                 if (lockOn.health > 0)
                 {
                     Vector2 tocalc = new Vector2(pos.X - lockOn.pos.X, pos.Y - lockOn.pos.Y);
+                      
                     tocalc.Normalize();
-                    tocalc *= 2;
-                    pos -= tocalc;
+                    tocalc *= 5;
+                    
+                    if (!initi)
+                    {
+                        tocalc = Vector2.Lerp(inertia, tocalc, .03f);
+                    }
+                    else
+                    {
+                        inertia = new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle));
+                        inertia *= (int)speed;
+                        tocalc = Vector2.Lerp(inertia, tocalc, .01f);
+                        initi = false;
+                    }
                     angle = Math.Atan2(tocalc.Y, tocalc.X);
+                 /*   inertia += tocalc;
+                    if (inertia.X > inertiafact)
+                    {
+                        inertia.X = inertiafact;
+                    }
+                    if (inertia.X < -inertiafact)
+                    {
+                        inertia.X = -inertiafact;
+                    }
+                    if (inertia.Y > inertiafact)
+                    {
+                        inertia.Y = inertiafact;
+                    }
+                    if (inertia.Y < -inertiafact)
+                    {
+                        inertia.Y = -inertiafact;
+                    }*/
+                    pos -= tocalc;
+                    //pos -= inertia;
+                    inertia = tocalc;
+                    
                  //   double angle2 = Math.Atan2(pos.Y, pos.X);
                   //  Console.WriteLine();
                 //    Console.WriteLine(angle);

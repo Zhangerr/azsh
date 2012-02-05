35a98c71335f8892ed6eb82446b0b16955d675a3
alex zhang <alexzhangh@gmail.com>
2012-02-05 10:03:35 -0800

    fixing..

---
+++ math/Config.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace math
+{
+   public class Config
+    {
+       public static Boolean mouseplay = false;
+    }
+}
